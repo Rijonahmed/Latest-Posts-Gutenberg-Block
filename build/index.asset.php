6f8a8d5e3f1a7b79579e8d33c05385fc3237f632
Rijonahmed <rijon036@gmail.com>
2023-11-09 23:32:04 +0600

    latest Posts code update

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '155ef5ff8cd6baa1689a931fc10fa901');
